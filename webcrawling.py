a04e14ccca9b45958cd297294558fd743fedeaaa
fasthill <kangeun@naver.com>
2021-10-04 23:08:20 +0900

    referer, user-agent, accept-language

--- webcrawling.py
+++ webcrawling.py
@@ -6,13 +6,19 @@ import re
 import time
 import os
 
-main_url = 'https://www.assembly.go.kr/assm/memact/congressman/memCond/memCond.do'
-
-html = requests.get(main_url)
+headers = {
+    'Referer' : 'https://www.assembly.go.kr/assm/memact/congressman/memCond/memCondListAjax.do'
+    # 'User-Agent':
+    #  http://useragentstring.com/
+    # 'Accept-Language':
+}
+main_url = 'https://www.assembly.go.kr/assm/memact/congressman/memCond/memCondListAjax.do'
+params = {'currentpage':1, 'rowPerPage': 300}
+html = requests.get(main_url, headers= headers)
 soup = bs(html.content, 'html.parser')
 
-list_ = soup.select('a[href*=jsMemPop]')
-# list_ = soup.select('a', {'href': re.compile(r'jsMemPop')})
+for idx, tag in enumerate(soup.select('a[href*=jsMemPop]'),1):
+    print('[{}] {}'.format(idx,tag.tet))
 
 # #themecast > div.theme_cosoup.select('div#themecast div.theme_head ul.theme_item')nt > div:nth-child(1) > div > ul > li:nth-child(1) > a.theme_info > strong
 print('1')
